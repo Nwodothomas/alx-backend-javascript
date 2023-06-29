7056a0a1bc0d6e7b0438960f0793496f9f561c00
Nwodo Thomas <Nwodothomas@gmail.com>
2023-06-29 23:31:55 +0000

    added 4-pricing.js file

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,38 +1,30 @@
-import Currency from './3-currency.js';
-
 export default class Pricing {
   constructor(amount, currency) {
     this._amount = amount;
     this._currency = currency;
   }
 
-  // Getter for amount
   get amount() {
     return this._amount;
   }
 
-  // Setter for amount
-  set amount(newAmount) {
-    this._amount = newAmount;
+  set amount(a) {
+    this._amount = a;
   }
 
-  // Getter for currency
   get currency() {
     return this._currency;
   }
 
-  // Setter for currency
-  set currency(newCurrency) {
-    this._currency = newCurrency;
+  set currency(c) {
+    this._currency = c;
   }
 
-  // Method to display the full price format
   displayFullPrice() {
-    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+    return `${this.amount} ${this.currency._name} (${this.currency._code})`;
   }
 
-  // Static method to convert price based on conversion rate
   static convertPrice(amount, conversionRate) {
-    return amount * conversionRate;
+    return (amount * conversionRate);
   }
 }
