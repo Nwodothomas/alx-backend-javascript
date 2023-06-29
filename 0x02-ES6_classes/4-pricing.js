79fdf17e77496b7a2686dbab88807d7dd34a3b5f
Nwodo Thomas <Nwodothomas@gmail.com>
2023-06-29 23:27:16 +0000

    added 4-pricing.js

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,38 @@
+import Currency from './3-currency.js';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // Getter for amount
+  get amount() {
+    return this._amount;
+  }
+
+  // Setter for amount
+  set amount(newAmount) {
+    this._amount = newAmount;
+  }
+
+  // Getter for currency
+  get currency() {
+    return this._currency;
+  }
+
+  // Setter for currency
+  set currency(newCurrency) {
+    this._currency = newCurrency;
+  }
+
+  // Method to display the full price format
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  // Static method to convert price based on conversion rate
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
