a8ca68336e42d69b193d48aabacb0d9266388b4a
Nwodo Thomas <Nwodothomas@gmail.com>
2023-06-30 00:00:47 +0000

    added 8-hbtn_class.js 9-hoisting.js and 10-car.js

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -4,12 +4,10 @@ export default class HolbertonClass {
     this._location = location;
   }
 
-  '''Casting to Number'''
   valueOf() {
     return this._size;
   }
 
-  '''Casting to String'''
   toString() {
     return this._location;
   }
