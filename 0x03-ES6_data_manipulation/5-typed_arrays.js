e7c59983e514c39d78a71fffefb2f8a5ea9f91df
Nwodo Thomas <Nwodothomas@gmail.com>
2023-07-05 00:19:36 +0000

    added task7

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,15 +1,10 @@
-function createInt8TypedArray(length, position, value) {
-  const buffer = new ArrayBuffer(length);
-  const view = new DataView(buffer);
-
-  if (position >= 0 && position < length) {
-    view.setInt8(position, value);
-  } else {
-    throw new Error("Position outside range");
+export default function createInt8TypedArray(length, position, value) {
+  if (position < 0 || position >= length) {
+    throw Error('Position outside range');
   }
-
-  return view;
+  const newBuffer = new ArrayBuffer(length);
+  const int8 = new Int8Array(newBuffer, 0, length);
+  int8.set([value], position);
+  return new DataView(newBuffer);
 }
-
-export default createInt8TypedArray;
 
