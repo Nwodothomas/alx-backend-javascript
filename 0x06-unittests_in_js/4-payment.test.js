dedc8812f49e88245e896cc969f5ccc01e16a818
Nwodothomas <108361891+Nwodothomas@users.noreply.github.com>
2023-08-25 23:15:49 +0100

    added 4-payment.test.js

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,24 +1,25 @@
-const { assert } = require('chai');
+const { expect } = require('chai');
 const sinon = require('sinon');
-const sendPaymentRequestToApi = require('./4-payment');
+
 const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
 
-describe('sendPaymentRequestToApi', function () {
-  it('should call Utils.calculateNumber with SUM type and stub the result', function () {
-
-    const calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
-
-    const consoleLogSpy = sinon.spy(console, 'log');
-
+describe('sendPaymentRequestToApi', () => {
+  const consoleSpy = sinon.spy(console, 'log');
+  it('validates usage of Utils.calculateNumber', () => {
+    // Stub the function always return the same number 10
+    const calcNumStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    // run the function with the parameters
     sendPaymentRequestToApi(100, 20);
+    // check that the stub is being called with type = SUM, a = 100, and b = 20
+    expect(calcNumStub.calledWith('SUM', 100, 20)).to.be.true;
+    // check that the stub always return the same number 10
+    expect(calcNumStub.alwaysReturned(10)).to.be.true;
+    // check with spy that console.log is logging the correct message 
+    expect(consoleSpy.calledWith('The total is: 10')).to.be.true;
 
-    sinon.assert.calledOnce(calculateNumberStub);
-    sinon.assert.calledWithExactly(calculateNumberStub, 'SUM', 100, 20);
-
-    sinon.assert.calledOnce(consoleLogSpy);
-    sinon.assert.calledWithExactly(consoleLogSpy, 'The total is: 10');
-
-    calculateNumberStub.restore();
-    consoleLogSpy.restore();
+    calcNumStub.restore();
+    consoleSpy.restore();
   });
 });
+
