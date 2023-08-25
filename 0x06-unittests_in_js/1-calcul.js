3d5afacbff8c7294405d89e03ab1a87cb46b9b34
Nwodothomas <108361891+Nwodothomas@users.noreply.github.com>
2023-08-25 20:56:42 +0100

    added 1-calcul.js

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,26 +1,18 @@
-/**
- * Perform mathematical operations on two numbers based on the type.
- * @param {string} type - The type of operation (SUM, SUBTRACT, DIVIDE).
- * @param {number} a - The first number.
- * @param {number} b - The second number.
- * @returns {number|string} - The result of the operation or 'Error' if division by zero.
- */
 function calculateNumber(type, a, b) {
-  const roundedA = Math.round(a);
-  const roundedB = Math.round(b);
-
-  switch (type) {
-    case 'SUM':
-      return roundedA + roundedB;
-    case 'SUBTRACT':
-      return roundedA - roundedB;
-    case 'DIVIDE':
-      if (roundedB === 0) {
-        return 'Error';
-      }
-      return roundedA / roundedB;
-    default:
-      throw new Error('Invalid type. Use SUM, SUBTRACT, or DIVIDE.');
+  const rounded_a = Math.round(a);
+  const rounded_b = Math.round(b);
+  
+  if (type === 'SUM') {
+    return rounded_a + rounded_b;
+  }
+  if (type === 'SUBTRACT') {
+    return rounded_a - rounded_b;
+  }
+  if (type === 'DIVIDE') {
+    if (rounded_b === 0) {
+      return 'Error';
+    }
+    return rounded_a / rounded_b;
   }
 }
 
