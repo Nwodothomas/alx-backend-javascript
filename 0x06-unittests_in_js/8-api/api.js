58f383f35c0af574d9ab38f64500f8e8668d9c8f
Nwodothomas <108361891+Nwodothomas@users.noreply.github.com>
2023-08-26 02:47:32 +0100

    added 8-api subdirectory

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,14 +1,14 @@
 const express = require('express');
 
 const app = express();
-const port = 7865;
+
+app.listen(7865, () => {
+  console.log('API available on localhost port 7865');
+});
 
 app.get('/', (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
   res.send('Welcome to the payment system');
 });
 
-app.listen(port, () => {
-  console.log(`API available on localhost port ${port}`);
-});
+module.exports = app;
+
